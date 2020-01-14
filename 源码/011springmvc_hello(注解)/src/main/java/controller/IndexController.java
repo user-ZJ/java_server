e1c4c8194c123cc90dfce2de97ef0629601f8276
zack <919353569@qq.com>
2020-01-14 20:28:55 +0800

    feature:添加springmvc注解方式

---
+++ 源码/011springmvc_hello(注解)/src/main/java/controller/IndexController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mav = new ModelAndView("index");
+        mav.addObject("message", "Hello Spring MVC");
+        return mav;
+    }
+}
